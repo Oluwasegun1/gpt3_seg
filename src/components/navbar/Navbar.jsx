f398aad5d364f596d44928d59ff99ff0b6f928ef
OluwaSegun Ogunbanjo <ogunbanjosegun@gmail.com>
2024-03-22 00:45:49 +0100

    Add assets and components for brand section, articles, CTA, features, navbar, blog, footer, header, possibility, and WhatGPT3

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {RiMenu3Line, RiCloseLin, RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
+import './navbar.css';
+
+const menu = () => {
+  <>
+  <p><a href='#home'>Home</a></p>
+  <p><a href='#wgpt3'>What is GPT3?</a></p>
+  <p><a href='#possibility'>Open AI</a></p>
+  <p><a href='#features'>Case Studies</a></p>
+  <p><a href='#blog'>Library</a></p>
+  </>
+}
+
+
+const Navbar = () => {
+const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className = "gpt3__navbar-links">
+        <div className = "gtp3__navbar-links_logo">
+           <img src={logo} alt='logo' />
+           </div>
+           <div className = "gpt3__navbar-links_containers">
+            <menu />
+
+           </div>
+      </div>
+      <div className = "gpt3__navbar-links_sign">
+        <p>Sign in</p>
+        <button type = "button" >Sign-Up</button>
+      </div>
+      <div className= "gpt3__navbar-menu">
+      {toggleMenu
+        ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false) } />
+        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true) } />
+      }
+      {toggleMenu && (
+        <div className='gpt3__navbar-menu_container scale-up-center'>
+          <div className='gpt2__navbar-menu_container-links'>
+          <menu />
+          </div>
+        </div>
+      )
+
+      }
+      
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './possibility.css';
+
+const Possibiblity = () => {
+  return (
+    <div>Possibiblity</div>
+  )
+}
+
+export default Possibiblity
