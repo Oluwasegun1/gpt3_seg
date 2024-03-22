6c5065e466bf615d063cf593ae341110da9b498e
OluwaSegun Ogunbanjo <ogunbanjosegun@gmail.com>
2024-03-22 08:13:53 +0100

    modified code, now navbar icons showing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
-import {RiMenu3Line, RiCloseLin, RiCloseLine} from 'react-icons/ri';
+import {RiMenu3Line,  RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
 
-const menu = () => {
+const Menu = () => (
   <>
   <p><a href='#home'>Home</a></p>
   <p><a href='#wgpt3'>What is GPT3?</a></p>
@@ -11,7 +11,7 @@ const menu = () => {
   <p><a href='#features'>Case Studies</a></p>
   <p><a href='#blog'>Library</a></p>
   </>
-}
+)
 
 
 const Navbar = () => {
@@ -24,7 +24,7 @@ const [toggleMenu, setToggleMenu] = useState(false);
            <img src={logo} alt='logo' />
            </div>
            <div className = "gpt3__navbar-links_containers">
-            <menu />
+            <Menu />
 
            </div>
       </div>
@@ -33,14 +33,14 @@ const [toggleMenu, setToggleMenu] = useState(false);
         <button type = "button" >Sign-Up</button>
       </div>
       <div className= "gpt3__navbar-menu">
-      {toggleMenu
+      {toggleMenu 
         ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false) } />
         : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true) } />
       }
       {toggleMenu && (
         <div className='gpt3__navbar-menu_container scale-up-center'>
-          <div className='gpt2__navbar-menu_container-links'>
-          <menu />
+          <div className='gpt3__navbar-menu_container-links'>
+          <Menu />
           </div>
         </div>
       )
